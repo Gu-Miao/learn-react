76766eba8f0e693e3f7781b05c7b2ef0c23d4d89
Gu-Miao <1450206741@qq.com>
2023-01-16 17:02:29 +0800

    Finish mini react

--- mini-react/src/index.jsx
+++ mini-react/src/index.jsx
@@ -1,13 +1,18 @@
 import React from './react'
 
 /** @jsx React.createElement */
-const element = (
-  <div id="foo">
-    <h1 style="color:red;">Bar</h1>
-    <br />
-    <p>asdfjalksdfjalsdjf</p>
-  </div>
-)
+function App() {
+  const [count, setCount] = React.useState(0)
+
+  return (
+    <div id="foo">
+      <h1 style="color:red;">Bar</h1>
+      <br />
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </div>
+  )
+}
 
 const container = document.getElementById('app')
-React.render(element, container)
+React.render(<App />, container)
